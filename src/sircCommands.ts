426da26d108f50d42c8a1b9b125cebded4bdf38d
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2020-06-14 14:11:02 -0400

    Messages have their own tabs

--- src/sircCommands.ts
+++ src/sircCommands.ts
@@ -22,6 +22,21 @@ export const msg = (client: Client, rawCommand: string): void => {
 
 	const ircArgs = [args[0], ':' + message];
 	client.sendCommand('PRIVMSG', ircArgs);
+
+	const indexOfRecipient = client.tabs.indexOf(args[0]);
+	console.log(indexOfRecipient);
+	if (indexOfRecipient !== -1) {
+		client.tab = indexOfRecipient;
+	} else {
+		client.tabs.push(args[0]);
+		client.tab = client.tabs.length - 1;
+		client.tabData.set(client.tab, []);
+	}
+
+	const tabData: Array<string> = [...client.tabData.get(client.tab)];
+	tabData.push(`\n${red(client.opts.nick)} ${blue('-->')} ${green(message)}`);
+	client.tabData.set(client.tab, tabData);
+	client.writeTabData();
 	// cursorTo(process.stdout, 0, process.stdout.rows - 1);
 	// process.stdout.write(
 	// 	`${red(client.opts.nick)} ${blue('-->')} ${green(message)}\n`,
